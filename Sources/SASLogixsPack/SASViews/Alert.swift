542e585885b7574896b0a137ecd6eb59e7d19d4b
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-06-21 11:18:37 +0530

    - 11:18 pm

---
+++ Sources/SASLogixsPack/SASViews/Alert.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sukumar Anup Sukumaran on 21/06/20.
+//
+
+import Foundation
+import UIKit
+
+
--- Sources/SASLogixsPack/String+Ext.swift
+++ Sources/SASLogixsPack/String+Ext.swift
@@ -1,5 +1,7 @@
 import Foundation
+#if !os(macOS)
 import UIKit
+#endif
 
 public extension String {
 
