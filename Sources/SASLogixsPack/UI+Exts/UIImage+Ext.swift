70f7dc84a9d20907dc6f8809f43f94627e44be92
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-24 17:09:51 +0530

    - added an extention for UIImage that adds a function to change the color of the image programmatically

---
+++ Sources/SASLogixsPack/UI+Exts/UIImage+Ext.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Manu Puthoor on 24/08/20.
+//
+
+import Foundation
+
+// usage -
+//SOME_IMAGE_VIEW.image = UIImage(named: "IMAGE NAME")?.maskWithColor(color: .white)
+//used if the rendering does not work in ios 12 .ie , if the image color does not change even if the tint color is altered. 
+extension UIImage {
+
+    func maskWithColor(color: UIColor) -> UIImage? {
+        let maskImage = cgImage!
+
+        let width = size.width
+        let height = size.height
+        let bounds = CGRect(x: 0, y: 0, width: width, height: height)
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+
+        context.clip(to: bounds, mask: maskImage)
+        context.setFillColor(color.cgColor)
+        context.fill(bounds)
+
+        if let cgImage = context.makeImage() {
+            let coloredImage = UIImage(cgImage: cgImage)
+            return coloredImage
+        } else {
+            return nil
+        }
+    }
+
+}
